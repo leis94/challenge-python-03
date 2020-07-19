de49f425942ad60703d0b953c249381ce6b695e8
Daniel <bukaro.dany@gmail.com>
2020-07-19 16:06:10 -0500

    resolve the challenge
    coding the def run function to find the encode message using regedex.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-# Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
-
+    message = ''
+    pattern = re.compile(r'([a-z\s?])')
+    f = open('encoded.txt', 'r', encoding='utf-8')
+    for line in f:
+        match = re.findall(pattern, line)
+        if match != "":
+             print(message.join(match))
+        else:
+            print('No encontro nada')
+    f.close()
 
 if __name__ == '__main__':
     run()
+
+
+
